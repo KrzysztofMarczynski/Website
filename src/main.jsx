c0f7d91da9894b8e68e245e36cf3024e5286d6e5
KrzysztofMarczyński <marczynskikrzysztof7@gmail.com>
2026-02-10 00:24:36 +0100

    minimal test white screen fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <div style={{
+      height: '100vh',
+      backgroundColor: '#111827',
+      color: 'white',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: '6rem',
+      fontWeight: 'bold',
+      textAlign: 'center',
+      padding: '20px'
+    }}>
+      DZIAŁA MINIMALNA WERSJA NA VERCEL
+    </div>
+  </StrictMode>
+);
