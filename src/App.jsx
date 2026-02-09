bdb5ce22fc51da4f9c27b5235cfdb0cd79495d13
KrzysztofMarczyński <marczynskikrzysztof7@gmail.com>
2026-02-10 00:51:10 +0100

    ogien

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ backgroundColor: '#111827', color: 'white', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <h1 style={{ fontSize: '6rem' }}>DZIAŁA NA VERCEL</h1>
-    </div>
-  );
-}
