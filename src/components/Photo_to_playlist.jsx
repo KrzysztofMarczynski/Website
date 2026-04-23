02f46c68e0c39dde5e0e535bd8cc53edbff92cbb
KrzysztofMarczyński <marczynskikrzysztof7@gmail.com>
2026-04-23 22:57:32 +0200

    create playlist

--- src/components/Photo_to_playlist.jsx
+++ src/components/Photo_to_playlist.jsx
@@ -68,19 +68,53 @@ export default function Print() {
       className="min-h-screen bg-gray-950 text-white p-10"
     >
       {/* TITLE */}
-      <motion.h1 className="text-5xl text-center mb-10 text-green-400">
-        Photo → Spotify Playlist
-      </motion.h1>
+      <motion.h2
+        initial={{ opacity: 0, y: 60 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.8 }}
+        className="text-4xl md:text-5xl lg:text-6xl font-bold text-center mb-16
+                     pb-6 md:pb-8 lg:pb-10 leading-[1.2] md:leading-[1.15] lg:leading-[1.1]
+                     bg-gradient-to-r from-blue-400 via-indigo-400 to-purple-500 
+                     bg-clip-text text-transparent drop-shadow-md"
+      >
+        Photo to Playlist
+      </motion.h2>
+
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.8, delay: 0.1 }}
+        className="max-w-3xl mx-auto text-center text-lg md:text-xl leading-relaxed text-gray-300 mb-20"
+      >
+        <p>
+          The program creates a Spotify playlist based on the photos you upload.
+          You can add up to three photos at a time, choose the genres you want
+          to include, your mood, and how many songs you want to include.
+        </p>
+        <br />
+        <p>Just Log in to your Spotify account, upload your photo.</p>
+      </motion.div>
 
       {/* LOGIN */}
       {!token && (
         <div className="text-center">
-          <button
-            onClick={loginSpotify}
-            className="bg-green-500 px-6 py-3 rounded-xl text-black font-bold"
-          >
-            Login with Spotify
-          </button>
+          <div className="max-w-6xl mx-auto flex flex-col cursor-pointer items-center justify-center text-center">
+            <a
+              onClick={loginSpotify}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-flex items-center gap-3 mt-6 px-8 py-4 
+                           bg-[#1DB954] 
+                           hover:bg-[#17a74a] 
+                           text-black font-medium text-lg rounded-xl transition-all 
+                           shadow-lg shadow-green-700/40 hover:shadow-green-600/60 
+                           active:scale-95"
+            >
+              Log in with Spotify
+            </a>
+          </div>
         </div>
       )}
 
